5b9b0c1113dd269d3a9b1bad1e22a76554afa5aa
imkzuma <98073935+imkzuma@users.noreply.github.com>
2022-06-15 23:45:08 +0800

    Update djikstra.h

--- djikstra.h
+++ djikstra.h
@@ -5,29 +5,28 @@ using namespace std;
 #define SOURCE 0
 
 const unsigned int Graph[V][V] = { 	{ 0 , 100 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 } ,
-									{ 100 , 0 , 200 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 } ,
-									{ 0 , 200  , 0 ,  180 , 0 , 0 , 0 , 0 , 0 , 0 , 550 , 0 , 0 , 0 , 0 , 0 , 0 , 0 } ,
-									{ 0 , 0 , 180 , 0 , 1500 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 } ,
-									{ 0 , 0 , 0 , 1500 , 0 , 260 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 } ,
-									{ 0 , 0 , 0 , 0 , 260 , 0 , 835 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 } ,
-									{ 0 , 0 , 0 , 0 , 0 , 835 , 0 , 150 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 } ,
-									{ 0 , 0 , 0 , 0 , 0 , 0 , 150 , 0 , 450 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 } ,
-									{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 450 , 0 , 250 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 } ,
-									{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 250 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 900 } ,
-									{ 0 , 0 , 550 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 2170 , 0 , 0 , 0 , 0 , 0 , 0 } ,
-									{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 2170 , 0 , 400 , 0 , 0 , 0 , 0 , 0 } ,
-									{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 400 , 0 , 375 , 0 , 0 , 0 , 0 } ,
-									{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 375 , 0 , 150 , 0 , 0 , 0 } ,
-									{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 150 , 0 , 450 , 0 , 0 } ,
-									{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 450 , 0 , 250 , 0 } ,
-									{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 250 , 0 , 900 } ,
-									{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 900 , 0 , 0 , 0 , 0 , 0 , 0 , 900 , 0 } };
+					{ 100 , 0 , 200 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 } ,
+					{ 0 , 200  , 0 ,  180 , 0 , 0 , 0 , 0 , 0 , 0 , 550 , 0 , 0 , 0 , 0 , 0 , 0 , 0 } ,
+					{ 0 , 0 , 180 , 0 , 1500 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 } ,
+					{ 0 , 0 , 0 , 1500 , 0 , 260 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 } ,
+					{ 0 , 0 , 0 , 0 , 260 , 0 , 835 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 } ,
+					{ 0 , 0 , 0 , 0 , 0 , 835 , 0 , 150 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 } ,
+					{ 0 , 0 , 0 , 0 , 0 , 0 , 150 , 0 , 450 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 } ,
+					{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 450 , 0 , 250 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 } ,
+					{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 250 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 900 } ,
+					{ 0 , 0 , 550 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 2170 , 0 , 0 , 0 , 0 , 0 , 0 } ,
+					{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 2170 , 0 , 400 , 0 , 0 , 0 , 0 , 0 } ,
+					{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 400 , 0 , 375 , 0 , 0 , 0 , 0 } ,
+					{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 375 , 0 , 150 , 0 , 0 , 0 } ,
+					{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 150 , 0 , 450 , 0 , 0 } ,
+					{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 450 , 0 , 250 , 0 } ,
+					{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 250 , 0 , 900 } ,
+					{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 900 , 0 , 0 , 0 , 0 , 0 , 0 , 900 , 0 } };
 
-const string namaJalan[V] = {	"Jl.Gatsu Tengah" , "Jl.Ayani" , "Jl.Kartini" , "Jl.Nakula" , "Jl. Werkudara" , "Jl.Arjuna" ,
-								"Jl.Sumatera" , "Jl. Hasanuddin" , "Jl.Sutoyo" , "Jl.Sudirman"};
+const string namaJalan[V] = {"Jl.Gatsu Tengah" , "Jl.Ayani" , "Jl.Kartini" , "Jl.Nakula" , "Jl. Werkudara" , "Jl.Arjuna" ,
+			     "Jl.Sumatera" , "Jl. Hasanuddin" , "Jl.Sutoyo" , "Jl.Sudirman"};
  
 int minDistance(int dist[], bool sptSet[]){
-   
     int min = INT_MAX, min_index;
  
     for (int v = 0; v < V; v++)
@@ -40,25 +39,24 @@ int minDistance(int dist[], bool sptSet[]){
 
 void printSolution(int dist[]){
 	int MAX = 0; const unsigned int kecepatan = 40;
-	
-    cout <<"Vertex \t    Jarak dari Awal" << endl;
-    
-    for (int i = 0; i < V; i++)
-    	i == 10 ? cout << endl << i << " \t\t" << dist[i] << endl : cout  << i << " \t\t"<<dist[i]<< endl;
+	cout <<"Vertex \t    Jarak dari Awal" << endl;
     	
-    cout << endl << "Jalan Terpendek dan Tercepat melalui Rute: ";
-    for(int i = 0; i < V; i++){
-    	i == 9 ? MAX = dist[i] : MAX = MAX;
-    	if(i < 10) {
-    		cout << i ; if(i < 9) cout << "->";
+	for (int i = 0; i < V; i++)
+    		i == 10 ? cout << endl << i << " \t\t" << dist[i] << endl : cout  << i << " \t\t"<<dist[i]<< endl;
+    	
+    	cout << endl << "Jalan Terpendek dan Tercepat melalui Rute: ";
+    	for(int i = 0; i < V; i++){
+		i == 9 ? MAX = dist[i] : MAX = MAX;
+    		if(i < 10) {
+    			cout << i ; if(i < 9) cout << "->";
 		}
-	}cout << endl;
+    	}cout << endl;
 	
 	cout << endl << "Nama Jalan yang dilewati adalah : " << endl;
 	for(int i = 0; i < V; i++){
-    	i == 9 ? MAX = dist[i] : MAX = MAX;
-    	if(i < 10) {
-    		cout << namaJalan[i]; if(i < 9) cout << " -> ";
+    		i == 9 ? MAX = dist[i] : MAX = MAX;
+    		if(i < 10) {
+    			cout << namaJalan[i]; if(i < 9) cout << " -> ";
 		}
 	}cout << endl;
 	
@@ -68,10 +66,7 @@ void printSolution(int dist[]){
 	cout << endl << "Tempat Awal : Lumintang" << endl;
 	cout << "Tempat Tujuan : Kampus Unud Sudirman" << endl;
 	cout << "Jarak Tempuh : " << Kilometer << " Km" << endl;
-	cout << "Estimasi Waktu Sampai : " << Est / 10 << " Menit" << endl;
-	
-	
-	
+	cout << "Estimasi Waktu Sampai : " << Est / 10 << " Menit" << endl;	
 }
  
 void dijkstra(const unsigned int graph[V][V], int src){
